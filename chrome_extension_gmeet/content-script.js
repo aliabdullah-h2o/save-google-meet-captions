9404cb89e7bf98dec61bfeb87569e89fcedea9d6
Ali Abdullah <ali.abdullah@cowlar.com>
2022-08-22 17:42:32 +0500

    add relevant class names

--- chrome_extension_gmeet/content-script.js
+++ chrome_extension_gmeet/content-script.js
@@ -1,14 +1,26 @@
+const rootCaptionsClass = "K6EKFb"
+const speakerClass = "TBMuR.bj4p3b"
+const speakerImageClass = "KpxDtd.r6DyN"
+const speakerNameClass = "zs7s8d.jxFHg"
+const speakerCaptionsParentClass = "Mz6pEf.wY1pdd"
+const speakerCaptionsClass = "iTTPOb.VbkSUe"
+
+
+
 var observerCC = new MutationObserver(function (mutations) {
+    let c = 0;
     mutations.forEach(function (mutation){
-        console.log(mutation.target.outerText);
+        console.log(c);
+        c = c+1;
+
     });
 });
 
 var observerMain = new MutationObserver(function (mutations) {
     mutations.forEach(function (mutation){
         console.log("ohohoshd");
-        if (mutation.target === document.querySelector(".K6EKFb")){
-            observerCC.observe(document.querySelector(".K6EKFb"), {attribute:false, subtree:true, attributeOldValue:false, characterDataOldValue:false, childList:false, characterData:false});
+        if (mutation.target === document.querySelector("."+rootCaptionsClass)){
+            observerCC.observe(document.querySelector("."+rootCaptionsClass), {attribute:false, subtree:true, attributeOldValue:false, characterDataOldValue:false, childList:true, characterData:false});
             observerMain.disconnect()    
         }
     });
